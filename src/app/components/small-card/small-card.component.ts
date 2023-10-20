8623ab810c8cedce94e15554dc067e53b913f497
Allan Mesquita <allan_santoz@hotmail.com>
2023-10-19 21:52:25 -0300

    Pegando os dados dinamicamente

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string = "https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  
+  @Input()
+  cardTitle:string = ""
+
+  @Input()
+  Id:string="0"
   constructor() { }
 
   ngOnInit(): void {
